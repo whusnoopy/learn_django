0c6efd8afe0cb91043f346ed3400f6c52d5e5b43
whusnoopy <whusnoopy@gmail.com>
2013-04-10 22:44:22 +0800

    Change some settings to make it can be viewed public

--- learn_django/urls.py
+++ learn_django/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.http import HttpResponseRedirect
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -7,6 +8,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'learn_django.views.home', name='home'),
+    url(r'^$', lambda x: HttpResponseRedirect('/polls/')),
     # url(r'^learn_django/', include('learn_django.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
@@ -16,3 +18,11 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^polls/', include('polls.urls', namespace="polls")),
 )
+
+from django.conf import settings
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.STATIC_ROOT,
+        }),
+    )
